fe210bd408d9715c5b4dd8d41d7871686165f1b5
Danilosrr <danilo_srr@hotmail.com>
2024-05-22 19:09:12 -0300

    logo, icons, estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Cadastro from "./components/Cadastro";
+import Receitas from "./components/Receitas";
+import ReceitasUsuario from "./components/ReceitasUsuario";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/receitas" element={<Receitas />} />
+        <Route path="/receitas/usuario" element={<ReceitasUsuario />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Receitas.jsx
@@ -0,0 +1,6 @@
+export default function Receitas(){
+    return (
+        <>
+        </>
+    )
+}
