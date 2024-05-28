2d8ccc9fa490c63be15a05a96c8ab0a0e9f956c0
opedrock <phklucio@gmail.com>
2024-05-28 16:15:44 -0300

    cadastro e login

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -1,35 +1,35 @@
 export default function Cadastro(){
     return (
-        <div className ="container">
-        <h2>Cadastro de Usuário</h2>
-        <form id="registrationForm" onsubmit="return validateForm()">
-            <label for="username">Nome de usuário:</label>
-            <input type="text" id="username" name="username" required>
+        <div className ="container bg">
+            <h2>Cadastro de Usuário</h2>
+            <form id="registrationForm" onsubmit="return validateForm()"/>
+                <label for="username">Nome de usuário:</label>
+                <input type="text" id="username" name="username" required/>
 
-            <label for="data de nascimento">data de nascimento:</label>
-            <input type="date" id="date name="data de nascimento" required>
+                <label for="data de nascimento">data de nascimento:</label>
+                <input type="date" id="date" name="data de nascimento" required/>
 
-            <label for="cpf">cpf</label>
-            <input type="text" id="cpf" name="cpf" maxlength="11" required>
+                <label for="cpf">cpf</label>
+                <input type="text" id="cpf" name="cpf" maxlength="11" required/>
 
-            <label for="endereço">endereço:</label>
-            <input type="location" id="endereço" name="endereço" required>
+                <label for="endereço">endereço:</label>
+                <input type="location" id="endereço" name="endereço" required/>
 
-            <laber for="telefone">telefone</laber>
-            <input type="telefone" id=" telefone" name="telefone" required>
+                <laber for="telefone">telefone</laber>
+                <input type="text" id=" telefone" name="telefone" required/>
 
-            <label for="sexo"> escolha o sexo: </label>
-            <select id="sexo" name="sexo">
-                <option
-                value ="M">masculino</option>
-                <option
-                value ="F">feminino</option>
-            </select>
+                <label for="sexo"> escolha o sexo: </label>
+                <select id="sexo" name="sexo">
+                    <option
+                    value ="M">masculino</option>
+                    <option
+                    value ="F">feminino</option>
+                </select>
 
-            <input type="submit" value="Cadastrar">
-        </form>
-    </div>
+                <button type="submit">Cadastrar</button>
+            <form/>
+        </div>
             
-        </input>
+        
     )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login(){
+
+    const navigate=useNavigate()
+
     return (
         <>
-        <div className="login-container">
-        <h2>login</h2>
-        <form id="loginForm">
-            <label for="username">Username:</label>
-            <input type="text" id="username" required>
-            <label for="cpf">cpf:</label>
-            <input type="cpf" id="cpf" required>
-            <button type="button" onclick="checklogin()">login</button>
-            <a href="file:///C:/Users/Henrique/Desktop/receitas/cadastro.html>
-            <button type="button ","onclick="cadastre">cadastre</button>
-        </form>
-    </div>
+        <div className="login-container bg">
+            <h2>login</h2>
+            <form id="loginForm">
+                <label for="username">Username:</label>
+                <input type="text" id="username" required/>
+                <label for="cpf">cpf:</label>
+                <input type="cpf" id="cpf" required/>
+                <button onClick="checklogin()">Login</button>
+                <button onClick={()=>{navigate("/cadastro")}}>Cadastro</button>
+            </form>
+        </div>
         </>
     )
 }
