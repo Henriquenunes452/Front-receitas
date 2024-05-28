c0c446197215131a00e0f59b242d2741502f2635
opedrock <phklucio@gmail.com>
2024-05-28 12:44:28 -0300

    categoria doces e salgadas

--- src/components/ReceitasDoces.jsx
+++ src/components/ReceitasDoces.jsx
@@ -1,23 +1,238 @@
+import temperos from "../assets/imgs/temperos.png";
+import comida1 from "../assets/imgs/imgcomida1.png";
+import empadinha from "../assets/imgs/empadinha.png";
+import coxinha from "../assets/imgs/coxinhafrango.png";
+import frango from "../assets/imgs/frangofritoimg.png";
+import { useEffect } from "react";
+
+
 export default function ReceitasDoces(){
+
+    useEffect(()=>{
+        window.scrollTo(0,0);
+    },[])
+
     return (
         <div className="bg">
+
+
             <header  id="home">
-        <div className="container">
-            <div className="menu">
-                <div className="logo"></div>
-                <div className="desktop-menu">
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">Sobre</a></li>
-                        <li><a href="#receitas">Receitas</a></li>
-                        <li><a href="#">Dicas</a></li>
-                        <li><a href="#">Contato</a></li>
-                    </ul>
+                <div className="containersec">
+                    <div className="menu">
+                        <div className="logo"></div>
+                        <div className="desktop-menu">
+                            <ul>
+                                <li><a href="#home">Home</a></li>
+                                <li><a href="#about">Sobre</a></li>
+                                <li><a href="#receitas">Receitas</a></li>
+                                <li><a href="#">Dicas</a></li>
+                                <li><a href="#">Contato</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+                <button id="back-to-top">^</button>
+          </header>
+
+            <div className="titulo">
+
+                <span className="text-title color-laranja">Receitas</span>
+                <h2 className="text-md color-cinza-1">Receitas Salgadas</h2>
+
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div className="receitassalgadas">
+
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Empadinha de camarão</h2>
+                            <img className="fts" src={empadinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Coxinha de frango</h2>
+                            <img className="fts" src={coxinha} alt=""/>
+                        </button>
+                    </div>
+                    <div className="box">
+                        <button className="rctslg">
+                            <h2>Frango frito</h2>
+                            <img className="fts" src={frango} alt=""/>
+                        </button>
+                    </div>
+                </div>
+
+
             </div>
-        </div>
-        <button id="back-to-top">^</button>
-    </header>
+
+
         </div>
     )
 }
