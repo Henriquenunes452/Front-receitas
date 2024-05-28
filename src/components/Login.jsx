0f0b893cba4771e992f657d919f867d99fe8a906
Henriquenunes452 <henriquen452@gmail.com>
2024-05-28 13:11:40 -0300

    tela de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,18 @@
 export default function Login(){
     return (
         <>
+        <div class="login-container">
+        <h2>login</h2>
+        <form id="loginForm">
+            <label for="username">Username:</label>
+            <input type="text" id="username" required>
+            <label for="cpf">cpf:</label>
+            <input type="cpf" id="cpf" required>
+            <button type="button" onclick="checklogin()">login</button>
+            <a href="file:///C:/Users/Henrique/Desktop/receitas/cadastro.html>
+            <button type="button" onclick="cadastre">cadastre</button>
+        </form>
+    </div>
         </>
     )
 }
