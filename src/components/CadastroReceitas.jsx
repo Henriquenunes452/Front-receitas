df075416790e8f2cf5f6455d99f9719a7f6c7d7e
opedrock <phklucio@gmail.com>
2024-05-28 20:48:59 -0300

    cadastro de receitas

--- src/components/CadastroReceitas.jsx
+++ src/components/CadastroReceitas.jsx
@@ -13,9 +13,45 @@ export default function CadastroReceitas(){
     },[])
 
     return (
-        <div className="bg">
+        <div className="cadastro bg">
 
-            
+            <div className="cadastrouser">
+
+            <h2>Cadastre aqui!</h2>    
+
+            <form action="">
+
+                <label className="test" for="nome">Nome da receita:</label>
+                <input type="text" id="nome" riquired/>
+
+                <label className="test" for="descricao">Descrição da receita:</label>
+                <input type="text" id="descricao" riquired/>
+
+                <label className="test" for="ingredientes">Ingredientes:</label>
+                <input type="text" id="ingredientes" riquired/>
+
+                <label className="test" for="preparo">Modo de preparo:</label>
+                <input type="text" id="preparo" riquired/>
+
+                <label className="test" for="img">Imagem da receita:</label>
+                <input type="file" name="arquivos" class="btn btn-success" multiple/>
+
+                <label className="test" for="nome">Nome da receita:</label>
+                <input type="text" id="nome" riquired/>
+
+                <div className="botaoreturn">
+
+
+                    <button onClick="checklogin()">Cadastrar</button>
+                    <button onClick={()=>{navigate("/receitas")}}>Voltar</button>
+
+
+                </div>
+
+
+            </form>
+
+            </div>
 
         </div>
     )
